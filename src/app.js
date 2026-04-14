582c559e0384d26910117278cdce08987d5cf96d
zAxn1e <itzsavekung123@gmail.com>
2026-04-15 00:15:20 +0700

    chore: update CORS configuration

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   allowedHeaders: ["Content-Type", "x-api-key", "Authorization"],
 }));
 app.use(express.json());
